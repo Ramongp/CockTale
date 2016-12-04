f086bc14c423fde06c20f148f5271eef4a810e58
DavidEmetres <david.martinez.martin.1995@gmail.com>
2016-12-04 19:59:48 +0100

    First & Ending Scene

---
+++ Assets/Code/EggNameBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class EggNameBehaviour : MonoBehaviour {
+
+	Text nameEditor;
+
+	void Start () {
+		nameEditor = GetComponent<Text> ();
+	}
+	
+	void Update () {
+		
+	}
+}
